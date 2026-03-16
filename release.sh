2c8485a16fe41ebf1387aba639c4cc4a0538da8a
Lucas Fernández Iglesias <lucasfdeziglesias24@gmail.com>
2026-03-16 12:28:42 +0100

    Archivos para la subida a la web

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# build-deploy.sh
+
+# Ir a la carpeta del proyecto Angular
+cd user-app || exit
+
+# Build producción apuntando a ../docs
+ng build --configuration production --output-path ../docs --base-href ./
+
+# Mover contenido de docs/browser a docs/
+if [ -d ../docs/browser ]; then
+  mv ../docs/browser/* ../docs/
+  rm -rf ../docs/browser
+fi
+
+# Crear archivo .nojekyll
+touch ../docs/.nojekyll
+
+echo "Build completado y listo en /docs"
